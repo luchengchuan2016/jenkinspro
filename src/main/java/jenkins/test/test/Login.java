e7cf1c7c7cad66863e73b4dd29594065cf5aa355
lcc <lcc@timevale.com>
2016-01-08 15:27:42 +0800

    t

---
+++ src/main/java/jenkins/test/test/Login.java
@@ -0,0 +1,21 @@
+/**
+ * 杭州天谷信息科技有限公司源代码，版权归杭州天谷信息科技有限公司所有 <br/>
+ * 项目名称：jenkinstest <br/>
+ * 文件名：Test2.java <br/>
+ * 包：jenkins.test.test <br/>
+ */
+package jenkins.test.test;
+
+/**
+ * 类名：Test2.java <br/>
+ * 中文名：TODO <br/>
+ * 描述：TODO <br/>
+ * 修改历史： <br/>
+ * 1.[2016年1月8日下午3:25:56]创建文件 by lcc
+ */
+public class Login {
+
+    public String L(){
+        return "b";
+    }
+}
