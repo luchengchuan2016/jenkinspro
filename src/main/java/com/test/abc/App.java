3991b745694eaa221cb357ed252d7fa49d68429d
lcc <lcc@timevale.com>
2016-01-08 19:22:12 +0800

    test

---
+++ src/main/java/com/test/abc/App.java
@@ -0,0 +1,21 @@
+/**
+ * 杭州天谷信息科技有限公司源代码，版权归杭州天谷信息科技有限公司所有 <br/>
+ * 项目名称：jenkinstest <br/>
+ * 文件名：App.java <br/>
+ * 包：com.test.abc <br/>
+ */
+package com.test.abc;
+
+/**
+ * 类名：App.java <br/>
+ * 中文名：TODO <br/>
+ * 描述：TODO <br/>
+ * 修改历史： <br/>
+ * 1.[2016年1月8日下午7:21:49]创建文件 by lcc
+ */
+public class App {
+
+    public void app(){
+        System.out.println("");
+    }
+}
---
+++ src/test/java/com/test/abc/AppTest.java
@@ -0,0 +1,23 @@
+/**
+ * 杭州天谷信息科技有限公司源代码，版权归杭州天谷信息科技有限公司所有 <br/>
+ * 项目名称：jenkinstest <br/>
+ * 文件名：AppTest.java <br/>
+ * 包：com.test.abc <br/>
+ */
+package com.test.abc;
+
+import junit.framework.TestCase;
+
+/**
+ * 类名：AppTest.java <br/>
+ * 中文名：TODO <br/>
+ * 描述：TODO <br/>
+ * 修改历史： <br/>
+ * 1.[2016年1月8日下午7:21:21]创建文件 by lcc
+ */
+public class AppTest extends TestCase {
+
+    public void testApp(){
+        assertEquals(true, true);
+    }
+}
