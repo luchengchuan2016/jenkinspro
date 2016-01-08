20d1647041d07aac7a3e23af538d657b82b38c0d
lcc <lcc@timevale.com>
2016-01-08 15:43:23 +0800

    aa

---
+++ src/test/java/jenkins/ta/LoginTest.java
@@ -0,0 +1,25 @@
+/**
+ * 杭州天谷信息科技有限公司源代码，版权归杭州天谷信息科技有限公司所有 <br/>
+ * 项目名称：jenkinstest <br/>
+ * 文件名：LoginTest.java <br/>
+ * 包：jenkins.test.test <br/>
+ */
+package jenkins.ta;
+
+import jenkins.ta.Login;
+import junit.framework.TestCase;
+
+/**
+ * 类名：LoginTest.java <br/>
+ * 中文名：TODO <br/>
+ * 描述：TODO <br/>
+ * 修改历史： <br/>
+ * 1.[2016年1月8日下午3:26:22]创建文件 by lcc
+ */
+public class LoginTest extends TestCase {
+
+    public void testL(){
+        String a = new Login().L();
+        assertEquals("testApp返回结果期望不同，不是b", a, "b");
+    }
+}
