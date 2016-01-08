124c0edde9f6b88224e49b6548e1b24907fd1603
lcc <lcc@timevale.com>
2016-01-08 09:35:56 +0800

    test

---
+++ src/main/java/jenkins/test/test/App.java
@@ -0,0 +1,16 @@
+package jenkins.test.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public String  print(String a){
+        System.out.println( "Hello World!" );
+        return a;
+    }
+    public static void main( String[] args )
+    {
+    }
+}
